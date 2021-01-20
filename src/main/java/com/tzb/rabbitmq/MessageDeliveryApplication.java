715ffea4b5b31dcc7c41623ccb9b8cbd6550c402
zhatang <564858834@qq.com>
2021-01-20 16:06:42 +0800

    初始版本提交

---
+++ src/main/java/com/tzb/rabbitmq/MessageDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.tzb.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageDeliveryApplication.class, args);
+    }
+
+}
